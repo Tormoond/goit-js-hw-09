82ea4bbeaba0db635ce6ad9884da63e7b411565a
Your Name <112875685+Tormoond@users.noreply.github.com>
2024-08-04 11:14:43 +0200

    home work 9
    Up load home work

---
+++ src/js/2-form.js
@@ -0,0 +1,54 @@
+const form = document.querySelector('.feedback-form');
+const labels = document.querySelectorAll('.feedback-form label');
+const emailInput = form.querySelector('input[name="email"]');
+const messageTextarea = form.querySelector('textarea[name="message"]');
+const button = document.querySelector('button');
+
+labels.forEach(label => {
+    label.classList.add('form-label');
+});
+emailInput.classList.add('form-input');
+messageTextarea.classList.add('form-textarea');
+button.classList.add('form-button');
+
+let formData = {
+    email: "",
+    message: ""
+};
+
+function addToLocalStorage() {
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function fillFromLocalStorage() {
+    const savedData = localStorage.getItem('feedback-form-state');
+    if (savedData) {
+        formData = JSON.parse(savedData);
+        emailInput.value = formData.email.trim();
+        messageTextarea.value = formData.message.trim();
+    }
+}
+
+form.addEventListener('input', (event) => {
+    formData[event.target.name] = event.target.value.trim();
+    addToLocalStorage();
+});
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    formData.email = formData.email.trim();
+    formData.message = formData.message.trim();
+
+    if (!formData.email || !formData.message) {
+        alert('Please fill in all fields');
+    } else {
+        console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+        localStorage.removeItem('feedback-form-state');
+        formData = { email: "", message: "" };
+        form.reset();
+    } 
+});
+
+
+window.addEventListener('load', fillFromLocalStorage);
